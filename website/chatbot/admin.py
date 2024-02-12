629e1407ee0b2af892c3f286ad6549d78daa83c0
Bilal Hussain <35193483+bqhussain@users.noreply.github.com>
2024-02-11 16:22:25 -0800

    Delete website directory

--- website/chatbot/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import BotChat
-
-# Register your models here.
-admin.site.register(BotChat)
--- website/loginstuff/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.contrib.auth.models import User
-User._meta.get_field('email')._unique = True
-
-
-class LoginstuffConfig(AppConfig):
-    name = 'loginstuff'
-
